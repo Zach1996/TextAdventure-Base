fcf1ab4defd428a8ae7c27cae32ce707fceee481
Zachary Fong <zachary.fong1@marist.edu>
2014-11-04 15:17:57 -0500

    Experimenting with the DOM.

---
+++ game.js
@@ -0,0 +1,31 @@
+//Lab9
+//Name: Zachary Fong
+var action = document.querySelector('#action');
+var test = document.querySelector('#descrip');
+var perfAction = function(action) {
+    switch(action) {
+    case 'Seppku':
+      break;
+    case 'asphixiation':
+      break;
+    case 'Go North':
+      break;
+    case 'Go South':
+      break;
+    case 'Go West':
+      break;
+    case 'Go East':
+      break;
+    case 'Convulse and Die':
+      break;
+    case 'Give Up':
+      break;
+    case 'Ingest nearest bottle of Cyanide':
+      break;
+    }
+}
+var title= document.querySelector('header > h1');
+title.innerHTML = 'Tibetin Mastiff';
+
+var button = document.getElementById('runner');
+button.onclick = run
